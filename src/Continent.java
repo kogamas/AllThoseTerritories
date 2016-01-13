b2704c2daaded7cd5f7ed38ffec3b08d866c0659
kogamas <kogamas13@gmail.com>
2016-01-13 19:26:45 +0100

    added some methods and commentary changed ArrayList to List

--- src/Continent.java
+++ src/Continent.java
@@ -1,10 +1,11 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class Continent {
     private String name;
     private int bonus;
-    private ArrayList<Territory> territories = new ArrayList<>();
+    private List<Territory> territories = new LinkedList<>();
 
 
     public Continent(String name, int bonus) {
@@ -12,7 +13,27 @@ public class Continent {
         this.bonus = bonus;
     }
 
-    void addTerritory(Territory t) {
+    /**
+     * This method adds a Territory
+     * @param t This Terriory will be added to this continent
+     */
+    public void addTerritory(Territory t) {
         this.territories.add(t);
+    }
+
+    /**
+     * This method returns a List of all Territories within
+     * @return This is a List of all territories defining this continent
+     */
+    public List<Territory> getTerritories() {
+        return territories;
+    }
+
+    /**
+     * This method returns the bonus value of the continent
+     * @return This is the value of the contient
+     */
+    public int getBonus() {
+        return bonus;
     }
 }
