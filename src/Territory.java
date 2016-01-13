bfdf9a03217c030465b2a8f9d41f4a82b4c07932
kogamas <kogamas13@gmail.com>
2016-01-13 19:18:51 +0100

    added id added controlledByPlayer changed ArrayList to List added Methods and comments

--- src/Territory.java
+++ src/Territory.java
@@ -1,35 +1,93 @@
-import java.util.ArrayList;
+import java.awt.Point;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Territory {
     private String name;
     private Point capital;
-    private ArrayList<Patch> patches = new ArrayList<>();
-    private ArrayList<Territory> neighbours = new ArrayList<>();
+    private int id;
+    private List<Patch> patches = new LinkedList<>();
+    private List<Territory> neighbors = new LinkedList<>();
     private int controlledByPlayer = -1;
 
+    public Territory(String name) {
+        this.name = name;
+    }
 
     public Territory(String name, Point capital) {
         this.name = name;
         this.capital = capital;
     }
 
-    void addPatch(Patch p) {
+    /**
+     * This method adds a Capital to the Territory
+     * @param capital  This is the capital as a Point (java.awt.Point)
+     */
+   public void addCapital(Point capital) {
+        this.capital = capital;
+    }
+    /**
+     * This method adds a Patch of Land to the Territory
+     * @param p  This is a Patch of Land
+     */
+   public void addPatch(Patch p) {
         this.patches.add(p);
     }
-
-    void addNeighbour(Territory t) {
-        this.neighbours.add(t);
+    /**
+     * This method adds a Neighbor to this Territory
+     * @param t  This is a Territory added as a Neighbor
+     */
+   public void addNeighbor(Territory t) {
+        this.neighbors.add(t);
     }
-
-    void addNeighbour(Territory[] terArray) {
+    /**
+     * This method adds multible Neighbors to this Territory
+     * @param terArray  This is an array of Territories added as a Neighbors
+     */
+   public void addNeighbor(Territory[] terArray) {
         for (Territory t : terArray) {
             List<Territory> newList = Arrays.asList(terArray);
-            this.neighbours.addAll(newList);
+            this.neighbors.addAll(newList);
         }
     }
 
+    /**
+     *
+     * @return an array of Patches
+     */
+    public Patch[] getPatches() {
+        return patches.toArray(new Patch[patches.size()]);
+    }
+
+    /**
+     * This method is used to check who is controlling this Territory
+     * @return int This returns a int representation of the Player controlling this Territory
+     */
+    public int getControlledByPlayer() {
+        return controlledByPlayer;
+    }
+
+    /**
+     *
+     * @return int This returns the Id of this Territory
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * This method sets the Id of this Territory
+     * @param id This is the id which is set
+     */
+    public void setId(int id){
+        this.id = id;
+    }
+
+    /**
+     * This method changes the controlling Player
+     * @param player This is the new Player controlling this Territory
+     */
     void setControlledByPlayer(int player) {
         this.controlledByPlayer = player;
     }
