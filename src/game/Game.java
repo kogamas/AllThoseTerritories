c02f3b4a403dd6ec5ba3c924f2fa6872ea438159
kogamas <kogamas13@gmail.com>
2016-01-13 20:43:06 +0100

    renamed Game.java to GameIF.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,36 +1,7 @@
 package game;
 
-public interface Game {
-    /**
-     * methods for drawing the map
-     */
-    void createMap();   //creates a World class (Map) for playing the game
-    void getMap();      //returns the points of the borders of the territories
-    void getNeighbors();    //returns all Pairs pairs of neighbors there are
 
-    /**
-     * Game related methods
-     */
-    void startGame();
-    boolean isGameOver();
-    void nextPhase();
-    void nextRound();   //could check if GameOver
+public class Game {
+    private World gameMap;
 
-    /**
-     * Army related methods
-     */
-    void setArmy();     //sets an army on a territory
-    void calculateReinforcements();
-
-    void moveArmy(int fromId, int toId);    //could return boolean if move is allowed
-    void attack(int fromId, int toId);  //return must tell won, lost and how many casualties
-
-    /**
-     * miscellaneous methods
-     *
-     */
-    int rollDice(); //returns int between 1-6
-    void infoOfId(); //returns all necessery information to a specific Id (territory or continent)
-
-    //space for more!!!
 }
---
+++ src/game/GameIF.java
@@ -0,0 +1,36 @@
+package game;
+
+public interface GameIF {
+    /**
+     * methods for drawing the map
+     */
+    void createMap();   //creates a World class (Map) for playing the game
+    void getMap();      //returns the points of the borders of the territories
+    void getNeighbors();    //returns all Pairs pairs of neighbors ids there are
+
+    /**
+     * Game related methods
+     */
+    void startGame();
+    boolean isGameOver();
+    void nextPhase();
+    void nextRound();   //could check if GameOver
+
+    /**
+     * Army related methods
+     */
+    void setArmy();     //sets an army on a territory
+    void calculateReinforcements();
+
+    void moveArmy(int fromId, int toId);    //could return boolean if move is allowed
+    void attack(int fromId, int toId);  //return must tell won, lost and how many casualties
+
+    /**
+     * miscellaneous methods
+     *
+     */
+    int rollDice(); //returns int between 1-6
+    void infoOfId(); //returns all necessery information to a specific Id (territory or continent)
+
+    //space for more!!!
+}
