939c2678200d201c4162be4440bf5975bb682d26
kogamas <kogamas13@gmail.com>
2016-01-14 18:00:04 +0100

    created Player class

---
+++ src/game/Player.java
@@ -0,0 +1,11 @@
+package game;
+
+public class Player {
+    private String name;
+    private int reinforcement;
+
+    public Player(String name){
+        this.name = name;
+        this.reinforcement = 0;
+    }
+}
