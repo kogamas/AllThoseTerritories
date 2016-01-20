ad2a4031e886738d24f9cfb6b6c72986b0d5cf92
kogamas <kogamas13@gmail.com>
2016-01-20 20:18:51 +0100

    changed privileges

--- src/Board/Patch.java
+++ src/Board/Patch.java
@@ -1,19 +1,19 @@
-package game;
+package Board;
 
 import java.awt.Point;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Patch {
+class Patch {
     private List<Point> borders = new LinkedList<>();
 
-    public Patch() {}
+    Patch() {}
 
     /**
      * This method adds a Point to the patch of land
      * @param p This is the Point added (java.awt.Point)
      */
-    public void addPoint(Point p) {
+    void addPoint(Point p) {
         this.borders.add(p);
     }
 
@@ -21,7 +21,7 @@ public class Patch {
      *
      * @return the borders of a Patch as an Array of Points (java.awt.Point)
      */
-    public Point[] getBorders() {
+    Point[] getBorders() {
         return borders.toArray(new Point[borders.size()]);
     }
 }
