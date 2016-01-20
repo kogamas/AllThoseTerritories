70d3a2b8235c51888eeddb57a6aade0543b6e4da
kogamas <kogamas13@gmail.com>
2016-01-20 20:25:39 +0100

    created interface for world communication

---
+++ src/Board/Board.java
@@ -0,0 +1,24 @@
+package Board;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+
+public interface Board {
+
+    int getTerritoryId(Point p);
+    Map<Integer,List<Point[]>> getTerritoryPointArrayMap();
+    int getControllingPlayerId(int territoryId);
+    void setControllingPlayer(int territoryId, int playerId);
+    int getArmy(int territoryId);
+    void addArmy(int territoryId, int n);
+    void subArmy(int territoryId, int n);
+    public Map<Integer,Territory> getTerritoryMap();
+    void printTerritories();
+
+    boolean validTerritoryId(int territoryId);
+    String getName(int id);
+    public int calculateReinforcement(int playerId);
+    public boolean isNeighborOf(int territoryId, int neighborId);
+
+}
